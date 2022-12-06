bdfd937aa1cc4e1538ef5d69da37cb8e270d0992
Victor Wildner <vwildner@redhat.com>
2022-12-05 23:46:15 -0300

    feat: add bind example

---
+++ examples/addrinfo/bind.cpp
@@ -0,0 +1,26 @@
+#include <arpa/inet.h>
+#include <iostream>
+#include <memory.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main()
+{
+    int                socket_fd;
+    struct sockaddr_in server_address;
+
+    socket_fd = socket( AF_INET, SOCK_STREAM, 0 );
+
+    server_address.sin_family      = AF_INET;
+    server_address.sin_port        = htons( 80 );
+    server_address.sin_addr.s_addr = inet_addr( "10.12.110.57" );
+    memset( server_address.sin_zero, '\0', sizeof( server_address.sin_zero ) );
+
+    bind( socket_fd,
+          ( struct sockaddr * ) &server_address,
+          sizeof( server_address ) );
+}
