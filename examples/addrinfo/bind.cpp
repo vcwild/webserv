ba6c5658f8edbbadfe5f0a2afefdb86fd288db6e
Victor Wildner <vwildner@redhat.com>
2022-12-09 18:56:36 -0300

    fix: update bind

--- examples/addrinfo/bind.cpp
+++ examples/addrinfo/bind.cpp
@@ -8,6 +8,8 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#define MY_PORT 5555
+
 int main()
 {
     int                socket_fd;
@@ -16,11 +18,12 @@ int main()
     socket_fd = socket( AF_INET, SOCK_STREAM, 0 );
 
     server_address.sin_family      = AF_INET;
-    server_address.sin_port        = htons( 80 );
+    server_address.sin_port        = htons( MY_PORT );
     server_address.sin_addr.s_addr = inet_addr( "10.12.110.57" );
     memset( server_address.sin_zero, '\0', sizeof( server_address.sin_zero ) );
 
     bind( socket_fd,
           ( struct sockaddr * ) &server_address,
           sizeof( server_address ) );
+    return 0;
 }
