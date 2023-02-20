6a47235a6a531b506f7b268d14bf6a110cab8229
Italo A <italoaurelior@gmail.com>
2023-02-20 19:01:18 -0300

    refactor: get request with mime, status and function to read file

--- sources/response/responseGet.cpp
+++ sources/response/responseGet.cpp
@@ -10,20 +10,34 @@ std::string ft::Response::getPath( std::string uri )
     return path;
 }
 
+static void readFromAFile( std::string path, std::string &body )
+{
+    std::ifstream file( path.c_str() );
+    if ( file.is_open() ) {
+        std::string line;
+        while ( getline( file, line ) ) {
+            body.append( line );
+        }
+        file.close();
+    }
+}
+
 void ft::Response::handleGet()
 {
     std::string   index_path = getPath( request.uri );
     std::ifstream index_file( index_path.c_str() );
+
+    std::string extension
+        = index_path.substr( index_path.find_last_of( "." ) + 1 );
+    setContentType( mime_types.getMimeType( "." + extension ) );
+
     if ( index_file.is_open() ) {
-        std::string line;
-        while ( getline( index_file, line ) ) {
-            body.append( line );
-        }
-        index_file.close();
-        setStatusCode( "200 OK" );
+        readFromAFile( index_path, body );
+        setStatusCode( status_codes.getStatusCode( 200 ) );
     } else {
-        // Show default 404 page if none are provided
-        setStatusCode( "404" );
-        setBody( "404 Not Found" );
+        // Load default 404 page 404.html
+        std::string error_path = getPath( "/404.html" );
+        readFromAFile( error_path, body );
+        setStatusCode( status_codes.getStatusCode( 404 ) );
     }
 }
