b5eba1df32f61e27df6bb7c29205035979a1ec80
Victor Wildner <vwildner@redhat.com>
2023-02-20 11:52:06 -0300

    fix: change handler for cgi

--- cgi-bin/basic.py
+++ cgi-bin/basic.py
@@ -2,16 +2,26 @@
 
 import os
 
-print("Content-type: text/html")
-print()
+def set_params(script_name: str, path_info: str):
+    print("Content-type: text/html")
+    print()
 
-print("<html>")
-print("<head>")
-print("<title>CGI Script</title>")
-print("</head>")
-print("<body>")
-print("<h1>CGI Script</h1>")
-print("<p>SCRIPT_NAME: " + os.environ["SCRIPT_NAME"] + "</p>")
-print("<p>PATH_INFO: " + os.environ["PATH_INFO"] + "</p>")
-print("</body>")
-print("</html>")
+    print("<html>")
+    print("<head>")
+    print("<title>CGI Script</title>")
+    print("</head>")
+    print("<body>")
+    print("<h1>CGI Script</h1>")
+    print("<p>SCRIPT_NAME: " + script_name + "</p>")
+    print("<p>PATH_INFO: " + path_info + "</p>")
+    print("</body>")
+    print("</html>")
+
+def exec():
+    try:
+        set_params(os.environ["SCRIPT_NAME"], os.environ["PATH_INFO"])
+    except KeyError:
+        set_params("basic.py", "no path info")
+
+if __name__ == "__main__":
+    exec()
