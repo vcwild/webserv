38ef53536a642a082ba7e9d2bbadb13b4c768261
Matheus Vieira Cervieri <77451759+MatheusCervieri@users.noreply.github.com>
2023-03-04 10:03:18 -0300

    I create handleDelete function and 401 html page (#38)
    Co-authored-by: MatheusCervieri <Matheus Cervieri>

--- sources/response/responseDelete.cpp
+++ sources/response/responseDelete.cpp
@@ -29,3 +29,36 @@ In summary, when a client sends a DELETE request, the server should respond with
 an HTTP response that indicates whether the deletion was successful or not,
 using an appropriate HTTP status code and, optionally, a response body.
 */
+
+static void readFromAFile( std::string path, std::string &body )
+{
+    std::ifstream file( path.c_str() );
+    if ( file.is_open() ) {
+        std::string line;
+        while ( getline( file, line ) ) {
+            body.append( line );
+        }
+        file.close();
+    }
+}
+
+void ft::Response::handleDelete()
+{
+    std::string   index_path = getPath( request.uri );
+    std::ifstream index_file( index_path.c_str() );
+
+    if ( index_file.is_open() ) 
+    {
+        std::string error_path = getPath( "/401.html" );
+        readFromAFile( error_path, body );
+        setStatusCode( status_codes.getStatusCode( 401 ) );
+        index_file.close(); 
+    }
+    else
+    {
+        std::string error_path = getPath( "/404.html" );
+        readFromAFile( error_path, body );
+        setStatusCode( status_codes.getStatusCode( 404 ) );
+    }
+    
+}
