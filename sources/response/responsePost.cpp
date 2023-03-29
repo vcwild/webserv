d7dd836a5f24974a982b60a637f9cfa2265ec92c
Matheus Vieira Cervieri <77451759+MatheusCervieri@users.noreply.github.com>
2023-03-29 20:54:37 -0300

    Add file upload to post request (#53)
    * Add file upload to post handle
    
    * I added maintest to the tests
    
    ---------
    
    Co-authored-by: MatheusCervieri <Matheus Cervieri>

--- sources/response/responsePost.cpp
+++ sources/response/responsePost.cpp
@@ -14,6 +14,96 @@ std::string ft::Response::generateFileName()
     file_path += ".txt";
     return file_path;
 }
+bool isMultipartFormData(const std::string& content_type) {
+    return content_type.find("multipart/form-data") != std::string::npos;
+}
+
+std::string getMultipartFormDataBoundary(const std::string& content_type) {
+    std::string boundary_string("boundary=");
+    std::string::size_type pos = content_type.find(boundary_string);
+
+    if (pos == std::string::npos) {
+        return "";
+    }
+
+    pos += boundary_string.length();
+    return content_type.substr(pos);
+}
+
+
+std::map<std::string, std::string> extractMultipartFormDataParts(const std::string& body, const std::string& boundary) {
+    std::map<std::string, std::string> parts;
+    std::string::size_type pos = 0;
+    std::string::size_type boundary_len = boundary.length();
+
+    while (pos < body.length()) {
+        std::string::size_type start_pos = body.find(boundary, pos);
+        if (start_pos == std::string::npos) {
+            break;
+        }
+
+        start_pos += boundary_len;
+
+        std::string::size_type end_pos = body.find(boundary, start_pos);
+        if (end_pos == std::string::npos) {
+            break;
+        }
+
+        std::string part_data = body.substr(start_pos, end_pos - start_pos - 2);
+
+        std::string::size_type content_disp_pos = body.find("Content-Disposition: form-data;", pos);
+        if (content_disp_pos != std::string::npos) {
+            std::string::size_type name_pos = body.find("name=\"", content_disp_pos);
+            std::string::size_type name_end_pos = body.find("\"", name_pos + 6);
+            if (name_pos != std::string::npos && name_end_pos != std::string::npos) {
+                std::string part_name = body.substr(name_pos + 6, name_end_pos - name_pos - 6);
+                parts["name"] = part_name;
+            }
+
+            std::string::size_type filename_pos = body.find("filename=\"", content_disp_pos);
+            std::string::size_type filename_end_pos = body.find("\"", filename_pos + 10);
+            if (filename_pos != std::string::npos && filename_end_pos != std::string::npos) {
+                std::string part_filename = body.substr(filename_pos + 10, filename_end_pos - filename_pos - 10);
+                parts["filename"] = part_filename;
+            }
+
+            std::string::size_type content_type_pos = body.find("Content-Type:", content_disp_pos);
+            std::string::size_type content_type_end_pos = body.find("\r\n", content_type_pos + 13);
+            if (content_type_pos != std::string::npos && content_type_end_pos != std::string::npos) {
+                std::string part_content_type = body.substr(content_type_pos + 13, content_type_end_pos - content_type_pos - 13);
+                parts["content_type"] = part_content_type;
+            }
+
+            // Remove header information from part data
+            std::string::size_type data_pos = part_data.find("\r\n\r\n");
+            if (data_pos != std::string::npos) {
+                parts["data"] = part_data.substr(data_pos + 4);
+            }
+        }
+
+        pos = end_pos + boundary_len;
+    }
+
+    return parts;
+}
+
+
+void createFileFromFormData(const std::map<std::string, std::string>& parts, const std::string& root_dir) {
+    std::string name = parts.at("name");
+    std::string filename = parts.at("filename");
+    std::string data = parts.at("data");
+
+    std::string file_path = root_dir + "/uploads/" + filename;
+
+  
+    std::ofstream file(file_path.c_str(), std::ios::out | std::ios::binary);
+    if (file.is_open()) {
+        file.write(data.c_str(), data.length());
+        file.close();
+    } else {
+        logger.error("Error creating file on server: " + file_path);
+    }
+}
 
 void ft::Response::handlePost()
 {
@@ -26,26 +116,45 @@ void ft::Response::handlePost()
         setContentType( mime_types.getMimeType( ".html" ) );
         setBody( handler.get_response_body() );
     }
+    else
+    {
+        if (isMultipartFormData(request.content_type)) {
 
-    if ( request.content_type != "text/plain" ) {
-        setStatusCode( status_codes.getStatusCode( 200 ) );
-        return;
+        std::string boundary = getMultipartFormDataBoundary(request.content_type);
+        if (!boundary.empty()) 
+        {
+            std::map<std::string, std::string> parts = extractMultipartFormDataParts(request.body, boundary);
+
+            // Check if the uploaded file is of the correct type
+            if (parts.count("content_type") > 0 && parts["content_type"].find("text/plain") != std::string::npos) {
+                // Create the file if it's of the correct type
+                createFileFromFormData(parts, server_conf.root_dir);
+
+                // Set the response status code to 201 Created
+                setStatusCode(status_codes.getStatusCode(201));
+                setContentType(mime_types.getMimeType(".html"));
+                setBody("<html><body><h1>201 Created</h1></body></html>");
+            } else {
+                // If the uploaded file is of the wrong type, return a 400 Bad Request error
+                setStatusCode(status_codes.getStatusCode(400));
+                setContentType(mime_types.getMimeType(".html"));
+                setBody("<html><body><h1>400 Bad Request</h1></body></html>");
+            }
+
+        } else {
+            // If it's not a multipart/form-data request with a valid boundary, return a 400 Bad Request error
+            setStatusCode(status_codes.getStatusCode(400));
+            setContentType(mime_types.getMimeType(".html"));
+            setBody("<html><body><h1>400 Bad Request</h1></body></html>");
+        }
+    } 
+        else 
+        {
+            // If it's not a multipart/form-data request, return a 202 Accepted status code indicating the request is received but not processed
+            setStatusCode(status_codes.getStatusCode(202));
+            setContentType(mime_types.getMimeType(".html"));
+            setBody("<html><body><h1>202 Accepted</h1></body></html>");
+        }
     }
 
-    std::string   file_path = generateFileName();
-    std::ofstream file( file_path.c_str() );
-
-    if ( !file.is_open() ) {
-        setStatusCode( status_codes.getStatusCode( 500 ) );
-        setContentType( mime_types.getMimeType( ".html" ) );
-        setBody(
-            "<html><body><h1>500 Internal Server Error</h1></body></html>" );
-        return;
-    }
-
-    file << request.body;
-    file.close();
-    setStatusCode( status_codes.getStatusCode( 201 ) );
-    setContentType( mime_types.getMimeType( ".html" ) );
-    setBody( "<html><body><h1>201 Created</h1></body></html>" );
 }
